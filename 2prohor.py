1cf255b4cfb7a3b9d8ffa5f9fb6a9299b1b5e4ba
prohor <67743726+prohor-n@users.noreply.github.com>
2025-10-14 16:57:44 +0400

    Update 2prohor.py

--- 2prohor.py
+++ 2prohor.py
@@ -6,7 +6,6 @@ def process(f):
     pattern = re.findall(r'\b\d*2[0-3][13]\b', txt)
     valid = [x for x in pattern if int(x, 4) <= 4095]
     for x in valid: print(x.replace('2',''))
-    if valid:
-        decs = [int(x,4) for x in valid]
-        print("Среднее (прописью):", to_words((min(decs)+max(decs))//2))
+    if valid: print("Среднее (прописью):", to_words((int(min(valid),4)+int(max(valid),4))//2))
 process("prohortest1.txt")
+
