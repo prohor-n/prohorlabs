503e69ea33b7348c73d28edfefdaa644cd24b767
prohor <67743726+prohor-n@users.noreply.github.com>
2025-11-24 18:35:31 +0400

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,4 +1,4 @@
-#1.	F(n<5)=10 ; F(n)= ((-1)^n)*(5*F(n-1)-7*F(n-2)+F(n//5))/((2n)!)  (при n >12), F(n)=F(n-2)-F(n-1) (при 5<n<=12)
+#1.	F(n<=5)=10 ; F(n)= ((-1)^n)*(6*F(n-1)-7*F(n-2))/((2n)!)  (при n >12), F(n)=F(n-2)-F(n-1) (при 5<n<=12)
 #Область определения функции – натуральные числа. Написать программу сравнительного 
 # вычисления данной функции рекурсивно и итерационно (значение, время). 
 # вывести таблицу и график сравнения времени рекурсивного и итерационого метода 
@@ -9,24 +9,23 @@ import matplotlib.pyplot as plt
 def F_recursive(n: int) -> float:
     if n <= 5:
         return 10
-    if n <= 12:
-        return F_recursive(n-2) - F_recursive(n-1)
-    sign = 1 if n % 2 == 0 else -1
-    return sign*(5*F_recursive(n-1) - 7*F_recursive(n-2) + F_recursive(n//5)) / math.factorial(2*n)
+    elif 5 < n <= 12:
+        return (F_recursive(n-2) - F_recursive(n-1))
+    else:
+        return (((-1)**n) * (6*F_recursive(n-1) - 7*F_recursive(n-2)) / (math.factorial(2*n)))
 
 def F_iterative(n):
-    F = [0]*(n+1)
+    F = {}
     factorial_cache = 1
     for i in range(n+1):
         if i >= 1:
             factorial_cache *= (2*i - 1)*(2*i)
         if i <= 5:
             F[i] = 10
-        elif i <= 12:
+        elif 5 < i <= 12:
             F[i] = F[i-2] - F[i-1]
         else:
-            sign = 1 if i % 2 == 0 else -1
-            F[i] = sign * (5*F[i-1] - 7*F[i-2] + F[i//5]) / factorial_cache
+            F[i] = ((-1)**i) * (6*F[i-1] - 7*F[i-2]) / factorial_cache
     return F[n]
 
 results = [(n,
@@ -47,4 +46,8 @@ plt.title('Сравнение времени: рекурсия vs итераци
 plt.legend()
 plt.grid(True)
 plt.show()
+
+plt.grid(True)
+plt.show()
+
 
